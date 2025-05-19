d37b36deaab7b8c6397923baad74f81fdc848017
NeonCardinal <nvnnaidenov@gmail.com>
2025-05-20 00:40:19 +0300

    add

---
+++ 001.IntroductionToProgramming/007.PrintSequence/PrintSequence.cs
@@ -0,0 +1,20 @@
+﻿//Chapter 001, Task 007
+//20.05.2025, 00:28
+
+public class PrintSequence
+{
+    static void Main()
+    {
+        for(int i = 2; i <= 102; i++)
+        {
+            if(i % 2 == 0)
+            {
+                Console.WriteLine(i);
+            }
+            else
+            {
+                Console.WriteLine($"-{i}");
+            }
+        }
+    }
+}
