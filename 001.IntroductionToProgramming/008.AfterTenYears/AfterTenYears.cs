749c853e427da567137df11dcd65a3eba2230d80
NeonCardinal <nvnnaidenov@gmail.com>
2025-05-20 00:40:43 +0300

    add

---
+++ 001.IntroductionToProgramming/008.AfterTenYears/AfterTenYears.cs
@@ -0,0 +1,14 @@
+﻿//Chapter 001, Task 008
+//20.05.2025, 00:31
+
+public class AfterTenYears
+{
+    static void Main()
+    {
+        int age = int.Parse(Console.ReadLine());
+
+        age += 10;
+
+        Console.WriteLine(age);
+    }
+}
