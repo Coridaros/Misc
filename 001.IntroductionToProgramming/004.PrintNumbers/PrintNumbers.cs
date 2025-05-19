a87f412a391881191b395a7154befb0c897e546b
NeonCardinal <nvnnaidenov@gmail.com>
2025-05-20 00:39:15 +0300

    add

---
+++ 001.IntroductionToProgramming/004.PrintNumbers/PrintNumbers.cs
@@ -0,0 +1,12 @@
+﻿//Chapter 001, Task 004
+//20.05.2025, 00:22
+
+public class PrintNumbers
+{
+    static void Main()
+    {
+        Console.WriteLine(1);
+        Console.WriteLine(101);
+        Console.WriteLine(1001);
+    }
+}
