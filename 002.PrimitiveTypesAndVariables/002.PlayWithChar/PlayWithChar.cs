f61ce9fbe848e89c6f2e9632d05336c86a030c73
NeonCardinal <nvnnaidenov@gmail.com>
2025-06-07 01:51:37 +0300

    add

---
+++ 002.PrimitiveTypesAndVariables/002.PlayWithChar/PlayWithChar.cs
@@ -0,0 +1,12 @@
+﻿//Chapter 002, Task 005
+//07.06.2025, 00:11
+
+public class PlayWithChar
+{
+    static void Main()
+    {
+        char symbol = '\u0072';
+
+        Console.WriteLine(symbol);
+    }
+}
