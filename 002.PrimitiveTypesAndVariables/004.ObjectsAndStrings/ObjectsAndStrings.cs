620a9b156adbfee6116183abe94a5a616a7b4365
NeonCardinal <nvnnaidenov@gmail.com>
2025-06-07 01:52:26 +0300

    add

---
+++ 002.PrimitiveTypesAndVariables/004.ObjectsAndStrings/ObjectsAndStrings.cs
@@ -0,0 +1,14 @@
+﻿//Chapter 002, Task 007
+//07.06.2025, 00:16
+
+public class ObjectsAndStrings
+{
+    static void Main()
+    {
+        string strOne = "Hello";
+        string strTwo = "World";
+        object obj = strOne + " " + strTwo;
+
+        Console.WriteLine(obj);
+    }
+}
