0a59a44346b9a0aeef7c24d212ab881cc04fd8f0
NeonCardinal <nvnnaidenov@gmail.com>
2025-06-07 01:52:00 +0300

    add

---
+++ 002.PrimitiveTypesAndVariables/003.BoolPlay/BoolPlay.cs
@@ -0,0 +1,12 @@
+﻿//Chapter 002, Task 006
+//07.06.2025, 00:14
+
+public class BoolPlay
+{
+    static void Main()
+    {
+        bool isMale = true;
+
+        Console.WriteLine(isMale);
+    }
+}
