d33cc734361d01e92bb2fd80343c1e654e27319f
NeonCardinal <nvnnaidenov@gmail.com>
2025-06-07 01:53:31 +0300

    add

---
+++ 002.PrimitiveTypesAndVariables/007.SwapValues/SwapValues.cs
@@ -0,0 +1,23 @@
+﻿//Chapter 002, Task 013
+//07.06.2025, 00:23
+
+public class SwapValues
+{
+    static void Main()
+    {
+        int first = int.Parse(Console.ReadLine());
+        int second = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Първоначалните стойности");
+        Console.WriteLine(first);
+        Console.WriteLine(second);
+
+        int temp = first;
+        first = second;
+        second = temp;
+
+        Console.WriteLine("След размяната");
+        Console.WriteLine(first);
+        Console.WriteLine(second);
+    }
+}
