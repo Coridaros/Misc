8e429e5f330ac0128930acc87f411d2ff50814b1
NeonCardinal <nvnnaidenov@gmail.com>
2025-06-07 01:52:47 +0300

    add

---
+++ 002.PrimitiveTypesAndVariables/005.ObjectsAndStringsVTwo/ObjectsAndStringsVTwo.cs
@@ -0,0 +1,15 @@
+﻿//Chapter 002, Task 008
+//07.06.2025, 00:18
+
+public class ObjectsAndStrings
+{
+    static void Main()
+    {
+        string strOne = "Hello";
+        string strTwo = "World";
+        object obj = strOne + " " + strTwo;
+        string cast = obj.ToString();
+
+        Console.WriteLine(cast);
+    }
+}
