abaeb215d6453738fd9ccb6e63989db9cd5e72e8
NeonCardinal <nvnnaidenov@gmail.com>
2025-06-07 01:53:08 +0300

    add

---
+++ 002.PrimitiveTypesAndVariables/006.Firm/Firm.cs
@@ -0,0 +1,20 @@
+﻿//Chapter 002, Task 012
+//07.06.2025, 00:20
+
+public class Firm
+{
+    static void Main()
+    {
+        string firstName = "Kolo";
+        string secondName = "lolov";
+        int age = 28;
+        char gender = 'm';
+        long id = 1234567890;
+
+        Console.WriteLine(firstName);
+        Console.WriteLine(secondName);
+        Console.WriteLine(age);
+        Console.WriteLine(gender);
+        Console.WriteLine(id);
+    }
+}
