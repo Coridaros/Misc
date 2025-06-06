5cf2e4eb8f67a50b5b73f4c35e1c28e8376a249d
NeonCardinal <nvnnaidenov@gmail.com>
2025-06-07 01:51:10 +0300

    add

---
+++ 002.PrimitiveTypesAndVariables/001.HexVariable/HexVariable.cs
@@ -0,0 +1,12 @@
+﻿//Chapter 002, Task 004
+//07.06.2025, 00:09
+
+public class HexVariable
+{
+    static void Main()
+    {
+        int hexValue = 0x100;
+
+        Console.WriteLine(hexValue);
+    }
+}
