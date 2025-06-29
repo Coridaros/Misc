7df3e4cd43ffdd47205193d865ba1e00418f61be
NeonCardinal <nvnnaidenov@gmail.com>
2025-06-29 13:38:49 +0300

    add

---
+++ 003.OperatorsAndExpressions/004.ChekBit/ChekBit.cs
@@ -0,0 +1,22 @@
+﻿//Chapter 003, Task 004
+//29.06.2025, 13:28
+
+public class ChekDigit
+{
+    static void Main()
+    {
+        int number = int.Parse(Console.ReadLine());
+
+        bool isThree = (number & 8) != 0;
+
+        if(isThree)
+        {
+            Console.WriteLine(1);
+        }
+        else
+        {
+
+            Console.WriteLine(0);
+        }
+    }
+}
