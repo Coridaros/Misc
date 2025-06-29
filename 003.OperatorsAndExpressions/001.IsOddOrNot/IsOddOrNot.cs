602b8617c8cf699dec142eab85305234956682ef
NeonCardinal <nvnnaidenov@gmail.com>
2025-06-29 13:37:45 +0300

    add

---
+++ 003.OperatorsAndExpressions/001.IsOddOrNot/IsOddOrNot.cs
@@ -0,0 +1,21 @@
+﻿//Chapter 003, Task 001
+//29.06.2025, 13:10
+
+public class IsOddOrNot
+{
+    static void Main()
+    {
+        int number = int.Parse(Console.ReadLine());
+
+        bool isOdd = number % 2 != 0;
+
+        if(isOdd)
+        {
+            Console.WriteLine($"Числото {number} е нечетно.");
+        }
+        else
+        {
+            Console.WriteLine($"Числото {number} е четно.");
+        }
+    }
+}
