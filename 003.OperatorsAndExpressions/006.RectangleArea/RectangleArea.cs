3bcc8fe7c735f237d79fd47ab4666acbe32f33f1
NeonCardinal <nvnnaidenov@gmail.com>
2025-06-29 13:39:40 +0300

    add

---
+++ 003.OperatorsAndExpressions/006.RectangleArea/RectangleArea.cs
@@ -0,0 +1,15 @@
+﻿//Chapter 003, Task 006
+//29.06.2025, 13:34
+
+public class RectangleArea
+{
+    static void Main()
+    {
+        double a = double.Parse(Console.ReadLine());
+        double b = double.Parse(Console.ReadLine());
+
+        double area = a * b;
+
+        Console.WriteLine(area);
+    }
+}
