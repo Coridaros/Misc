ad250559eb4a9dc14e5110b56c1a8dc85db9714f
NeonCardinal <nvnnaidenov@gmail.com>
2025-06-29 13:38:26 +0300

    add

---
+++ 003.OperatorsAndExpressions/003.ChekDigit/ChekDigit.cs
@@ -0,0 +1,24 @@
+﻿//Chapter 003, Task 003
+//29.06.2025, 13:24
+
+public class ChekDigit
+{
+    static void Main()
+    {
+        Console.Write("Number = ");
+
+        int number = int.Parse(Console.ReadLine());
+
+        int d = number / 100;
+        int digit = number % 10;
+
+        if (digit == 7)
+        {
+            Console.WriteLine("Yes");
+        }
+        else
+        {
+            Console.WriteLine("No");
+        }
+    }
+}
