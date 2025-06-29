57deb5908162f6a4169b995cfa98a2d4b4caa21a
NeonCardinal <nvnnaidenov@gmail.com>
2025-06-29 13:38:05 +0300

    add

---
+++ 003.OperatorsAndExpressions/002.DivideByFiveAndSeven/DivideByFiveAndSeven.cs
@@ -0,0 +1,22 @@
+﻿//Chapter 002, Task 002
+//29.06.2025, 13:19
+
+public class DivideByFiveAndSeven
+{
+    static void Main()
+    {
+        Console.Write("Въведете число: ");
+        int number = int.Parse(Console.ReadLine());
+
+        bool isDivide = number % 5 == 0 && number % 7 == 0;
+
+        if(isDivide)
+        {
+            Console.WriteLine("Числото се дели едновременно на 5 и 7 без остатък.");
+        }
+        else
+        {
+            Console.WriteLine("Числото не се дели на 5 и 7 без остатък.");
+        }
+    }
+}
