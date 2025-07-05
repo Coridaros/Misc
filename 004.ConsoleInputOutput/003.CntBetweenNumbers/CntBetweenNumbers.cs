8e2652d49d869891d413269ae8e7468fd2c0b076
NeonCardinal <nvnnaidenov@gmail.com>
2025-07-05 23:23:36 +0300

    add

---
+++ 004.ConsoleInputOutput/003.CntBetweenNumbers/CntBetweenNumbers.cs
@@ -0,0 +1,23 @@
+﻿//Chapter 004, Task 003
+//29.06.2025, 17:11
+
+public class CntBetweenNumbers
+{
+    static void Main()
+    {
+        int cnt = 0;
+
+        int start = int.Parse(Console.ReadLine());
+        int end = int.Parse(Console.ReadLine());
+
+        for(int i = start; i <= end; i++)
+        {
+            if(i % 5 == 0)
+            {
+                cnt++;
+            }
+        }
+
+        Console.WriteLine(cnt);
+    }
+}
