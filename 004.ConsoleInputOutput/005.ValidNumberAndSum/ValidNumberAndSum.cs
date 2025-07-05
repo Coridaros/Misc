9c209f2a9e3bbcf86c1baee4f733164583aab016
NeonCardinal <nvnnaidenov@gmail.com>
2025-07-05 23:24:14 +0300

    add

---
+++ 004.ConsoleInputOutput/005.ValidNumberAndSum/ValidNumberAndSum.cs
@@ -0,0 +1,25 @@
+﻿//Chapter 004, Task 005
+//29.06.2025, 17:16
+
+public class ValidNumberAndSum
+{
+    static void Main()
+    {
+        int sum = 0;
+        //bool isValid = false;
+        int number;
+
+        for(int i = 0; i < 5; i++)
+        {
+            if(int.TryParse(Console.ReadLine(), out number) != true)
+            {
+                i--;
+                Console.WriteLine("Невалидно число!");
+            }
+
+            sum += number;
+        }
+
+        Console.WriteLine(sum);
+    }
+}
