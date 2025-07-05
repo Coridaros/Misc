36eded934ae143dcb3ab4c71f284ad3869555524
NeonCardinal <nvnnaidenov@gmail.com>
2025-07-05 23:25:13 +0300

    add

---
+++ 004.ConsoleInputOutput/008.PrintNumbers/PrintNumbers.cs
@@ -0,0 +1,15 @@
+﻿//Chapter 004, Task 008
+//29.06.2025, 17:25
+
+public class PrintNumbers
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for(int i = 1; i <= n; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
