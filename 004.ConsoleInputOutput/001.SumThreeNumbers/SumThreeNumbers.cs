88598d8146421af5e8edfbef8f05863bbc901579
NeonCardinal <nvnnaidenov@gmail.com>
2025-07-05 23:22:55 +0300

    add

---
+++ 004.ConsoleInputOutput/001.SumThreeNumbers/SumThreeNumbers.cs
@@ -0,0 +1,16 @@
+﻿//Chapter 004, Task 001
+//29.06.2025, 17:06
+
+public class SumThreeNumbers
+{
+    static void Main()
+    {
+        int firstNumber = int.Parse(Console.ReadLine());
+        int secondNumber = int.Parse(Console.ReadLine());
+        int thirdNumber = int.Parse(Console.ReadLine());
+
+        int sum = firstNumber + secondNumber + thirdNumber;
+
+        Console.WriteLine(sum);
+    }
+}
