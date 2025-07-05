dbd135f74696ac079e2a9e4840c8aad463430d39
NeonCardinal <nvnnaidenov@gmail.com>
2025-07-05 23:23:17 +0300

    add

---
+++ 004.ConsoleInputOutput/002.CircleAreaAndPerimeter/CircleAreaAndPerimeter.cs
@@ -0,0 +1,16 @@
+﻿//Chapter 004, Task 002
+//29.06.2025, 17:08
+
+public class CircleAreaAndPerimeter
+{
+    static void Main()
+    {
+        double r = double.Parse(Console.ReadLine());
+
+        double area = Math.PI * r * r;
+        double perimeter = 2 * Math.PI * r;
+
+        Console.WriteLine(area);
+        Console.WriteLine(perimeter);
+    }
+}
