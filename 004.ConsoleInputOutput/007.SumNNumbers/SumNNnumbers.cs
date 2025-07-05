e8de44a4347c36957be8a7c389c900ba2fddf32d
NeonCardinal <nvnnaidenov@gmail.com>
2025-07-05 23:24:54 +0300

    add

---
+++ 004.ConsoleInputOutput/007.SumNNumbers/SumNNnumbers.cs
@@ -0,0 +1,19 @@
+﻿//Chapter 004, Task 007
+//29.06.2025, 17:27
+
+public class SumNNumbers
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int sum = 0;
+
+        for(int i = 0; i < n; i++)
+        {
+            sum += int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine(sum);
+    }
+}
