2af5ac1dc92bea88b8c96c35491f108ee18a0afa
NeonCardinal <nvnnaidenov@gmail.com>
2025-07-05 23:23:55 +0300

    add

---
+++ 004.ConsoleInputOutput/004.GreaterNumber/GreaterNumber.cs
@@ -0,0 +1,13 @@
+﻿//Chapter 004, Task 004
+//29.06.2025, 17:14
+
+public class GreaterNumber
+{
+    static void Main()
+    {
+        int firstNumber = int.Parse(Console.ReadLine());
+        int secondNumber = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(Math.Max(firstNumber, secondNumber));
+    }
+}
